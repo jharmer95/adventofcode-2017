99118f631123eb8b5bae114717098bd43266181c
Jackson Harmer <jharmer95@gmail.com>
2019-06-03 21:17:08 -0400

    Most of advent1_2 implemented, need to fix bug where last digit is not checked

--- 1/advent1_2.s
+++ 1/advent1_2.s
@@ -4,11 +4,13 @@
 // Registers used:
 // r0 - 0th parameter and return value
 // r1 - 1st parameter
-// r19 - 1st digit (used for wrap-around)
+// r19 - Gap size (1/2 * num of digits)
 // r20 - Accumulated sum (used for return value)
 // r21 - Current char offset
-// r22 - Previous digit
-// r23 - Current digit
+// r22 - Current digit
+// r23 - Comparing digit
+// r24 - Total size
+// r25 - Offset + gap
 .text
 
 .global main
@@ -19,53 +21,77 @@ main:
     mov x29, sp
     stp x19, x20, [sp, 16]
     stp x21, x22, [sp, 32]
-    str x23, [sp, 48]
-    str w0, [sp, 76]
-    str x1, [sp, 64]
+    stp x23, x24, [sp, 48]
+    str x25, [sp, 64]
 
     // Initialize sum to 0
     mov x20, #0
-    // Initialize offset to 1
-    mov x21, #1
+    // Initialize offset to 0
+    mov x21, #0
 
     // check if number of params is < 2 (aka no passed in params)
     cmp x0, #2
     b.lt fin
 
-    // Load first char of parameter1 to r19
-    ldr x19, [x1, 8]
-    ldrb w19, [x19]
-
-    // convert ASCII char to decimal digit
-    sub w19, w19, #48
-
-    // Store as previous digit
-    mov w22, w19
-
 // Loop until char is NULL
-digitLoop:
-    // Load next char of parameter1 to r23
-    ldr x23, [x1, 8]
-    ldrb w23, [x23, x21]
-    cmp w23, #0
-    b.eq done
+digitLoop1:
+    // Load next char of parameter1 to r22
+    ldr x22, [x1, 8]
+    ldrb w22, [x22, x21]
+    cmp w22, #0
+    b.eq endFound
+    // increment offset and loop again
+    add x21, x21, #1
+    b digitLoop1
 
+endFound:
+    mov x24, x21
+    mov x19, #2
+    // Copy offset / 2 to r19
+    udiv x19, x21, x19
+    // Reset offset
+    mov x21, #0
+
+digitLoop2:
+    // Load next char of parameter1 to r22
+    cmp x21, x24
+    b.gt done
+    ldr x22, [x1, 8]
+    ldrb w22, [x22, x21]
+    sub w22, w22, #48
+    add x25, x21, x19
+    // If desired location is greater or equal to max index, wrap around
+    cmp x25, x24
+    b.lt compare
+    sub x25, x24, x25
+
+compare:
+    ldr x23, [x1, 8]
+    ldrb w23, [x23, x25]
     sub w23, w23, #48
-    cmp w23, w22
+    cmp w22, w23
     b.ne skipSum
-    add w20, w20, w23
+    add w20, w20, w22
 
 skipSum:
-    // move current digit to prev. digit, increment offset, and loop again
-    mov w22, w23
+    // increment offset, and loop again
     add x21, x21, #1
-    b digitLoop
+    b digitLoop2
 
 done:
     cmp x21, #1
     b.eq fin
     // Compare last digit to first to detect wrap-around
-    cmp w22, w19
+    sub x21, x21, #1
+    ldr x22, [x1, 8]
+    ldrb w22, [x22, x21]
+    sub w22, w22, #48
+    add x25, x21, x19
+    sub x25, x24, x25
+    ldr x23, [x1, 8]
+    ldrb w23, [x23, x25]
+    sub w23, w23, #48
+    cmp w22, w23
     b.ne fin
     add w20, w20, w22
 
