b0a96ed5b31955c1aa262728db84bee93033723b
jharmer95 <jharmer95@gmail.com>
2019-07-11 13:02:23 -0400

    Finished Day5 (rust)

---
+++ Day5/part1/src/main.rs
@@ -0,0 +1,40 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Error, must provide a file to read from!");
+        return;
+    }
+    let mut vec = load_from_file(&args[1]);
+
+    let mut num_steps = 0;
+    let mut cur_pos = 0;
+
+    loop {
+        if cur_pos >= vec.len() || (cur_pos as i32) < 0 {
+            break;
+        }
+
+        let jump_num = vec[cur_pos];
+        vec[cur_pos] = jump_num + 1;
+        cur_pos = (cur_pos as i32 + jump_num) as usize;
+        num_steps += 1;
+    }
+
+    println!("Finished in {} steps!", num_steps);
+}
+
+fn load_from_file<P>(filename: P) -> Vec<i32> where P: AsRef<Path>, {
+    let mut vec: Vec<i32> = Vec::new();
+    let file = File::open(filename).unwrap();
+
+    for line in BufReader::new(file).lines() {
+        vec.push(line.unwrap().parse::<i32>().unwrap());
+    }
+
+    return vec;
+}
